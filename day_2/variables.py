822ecc993ad08d7f5a3f488837677573f09e680a
Abraham Ayomide Esther <iretiayo04@gmail.com>
2026-09-19 02:08:41 +0100

    Create variables.py

---
+++ day_2/variables.py
@@ -0,0 +1,12 @@
+# Day 2: 30 Days of python programming
+first_name = "Hadassah"
+last_name = "Esther"
+full_name = first_name + " " + last_name
+country = "Nigeria"
+city = "Lagos"
+age = 27
+year = 2026
+is_married = False
+is_true = True
+is_light_on = False
+a, b, c = 5, 10, 15
